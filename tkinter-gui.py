f9653d5c5d769f59444e59ab16d2eec22b5ba657
Aung Pyae <39703374+aungpyaeap@users.noreply.github.com>
2019-06-21 17:05:42 +0800

    Update tkinter-gui.py

--- tkinter-gui.py
+++ tkinter-gui.py
@@ -26,3 +26,68 @@ canvas1.create_window(200, 180, window=button1)
 
 root.mainloop()
 
+# -------------  New design ------------------------
+import tkinter as tk
+from tkinter import messagebox
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 400, height = 300,  relief = 'raised')
+canvas1.pack()
+
+label1 = tk.Label(root, text='Calculate the Square Root')
+label1.config(font=('helvetica', 14))
+canvas1.create_window(200, 25, window=label1)
+
+label2 = tk.Label(root, text='Type your Number:')
+label2.config(font=('helvetica', 10))
+canvas1.create_window(200, 100, window=label2)
+
+entry1 = tk.Entry (root) 
+canvas1.create_window(200, 140, window=entry1)
+
+def getSquareRoot ():
+    
+    x1 = entry1.get()
+    
+    if x1 == "":
+        messagebox.showinfo("Title", "PLease type a numerical value")
+    else:
+    
+        label3 = tk.Label(root, text= 'The Square Root of ' + x1 + ' is:',font=('helvetica', 10))
+        canvas1.create_window(200, 210, window=label3)
+    
+        label4 = tk.Label(root, text= float(x1)**0.5,font=('helvetica', 10, 'bold'))
+        canvas1.create_window(200, 230, window=label4)
+    
+button1 = tk.Button(text='Get the Square Root', command=getSquareRoot, bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+canvas1.create_window(200, 180, window=button1)
+
+
+root.mainloop()
+
+# -----------------------------------------  test ------------------------------
+import tkinter as tk
+from tkinter import messagebox
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 400, height = 300,  relief = 'raised')
+canvas1.pack()
+
+label1= tk.Label(root, text='Type your Number 1:')
+label1.config(font=('helvetica', 10))
+canvas1.create_window(200, 20, window=label1)
+
+entry1 = tk.Entry (root) 
+canvas1.create_window(200, 40, window=entry1)
+
+label2= tk.Label(root, text='Type your Number 2:')
+label2.config(font=('helvetica', 10))
+canvas1.create_window(200, 60, window=label2)
+
+entry2 = tk.Entry (root) 
+canvas1.create_window(200, 80, window=entry2)
+
+
+root.mainloop()
